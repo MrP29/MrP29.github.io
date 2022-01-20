7bdd346a92c399e0e9cfb1c529ae8da5cac75841
MrP29 <pokkm29@gmail.com>
2022-01-19 22:39:54 -0500

    change to vanila html

--- src/components/UI/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classes from "./Button.module.css";
-
-const Button = (props) => {
-  return <button>{props.children}</button>;
-};
-
-export default Button;
--- src/components/UI/CardItem.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const CardItem = (props) => {
-  return <div></div>;
-};
-
-export default CardItem;
